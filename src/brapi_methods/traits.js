48240ccc5a8cb754d669e57f07df47d338720b92
dauglyon <dauglyon@users.noreply.github.com>
2018-09-24 15:41:14 -0400

    added versioning/comments to seasons, studies, traits, trials, variables

--- src/brapi_methods/traits.js
+++ src/brapi_methods/traits.js
@@ -1,21 +1,39 @@
-/** GET /traits  */
-export function traits_list(params,behavior){
-    var behavior = behavior=="map"?behavior:"fork";
-    return this.brapi_call(behavior,"get",function(datum){
-        var datum_params = typeof params === "function" ? params(datum) 
-                            : Object.assign({}, params);
-        var url = "/traits";
-        return {'url':url, 'params':datum_params};
-    }, typeof params === "function");
+/** `GET /traits`
+ * @alias Context_Node.prototype.traits
+ * @param {Object} params Parameters to provide to the call
+ * @param {String} [behavior="fork"] Behavior of the node
+ * @return {BrAPI_Behavior_Node}
+ */
+export function traits (params,behavior){
+    var call = {
+        'defaultMethod': 'get',
+        'urlTemplate': '/traits',
+        'params': params,
+        'behaviorOptions': ['fork','map'],
+        'behavior': behavior,
+    }
+    this.version.check(call.urlTemplate,{
+        introduced:"v1.0"
+    });
+    return this.simple_brapi_call(call);
 }
 
-/** GET /traits/{traitDbId} */
-export function traits(params){
-    return this.brapi_call("map","get",function(datum){
-        var datum_params = typeof params === "function" ? params(datum) 
-                            : Object.assign({}, params);
-        var url = "/traits/"+datum_params.traitDbId;
-        delete datum_params.traitDbId;
-        return {'url':url, 'params':datum_params};
-    }, typeof params === "function");
+/** `GET /traits/{traitDbId}`
+ * @alias Context_Node.prototype.traits_detail
+ * @param {Object} params Parameters to provide to the call
+ * @param {Object} params.traitDbId traitDbId
+ * @param {String} [behavior="fork"] Behavior of the node
+ * @return {BrAPI_Behavior_Node}
+ */
+export function traits_detail (params,behavior){
+    var call = {
+        'defaultMethod': 'get',
+        'urlTemplate': '/traits/{traitDbId}',
+        'params': params,
+        'behavior': 'map',
+    }
+    this.version.check(call.urlTemplate,{
+        introduced:"v1.0"
+    });
+    return this.simple_brapi_call(call);
 }
--- src/brapi_methods/trials.js
+++ src/brapi_methods/trials.js
@@ -1,21 +1,39 @@
-/** GET /trials */
-export function trials_list(params,behavior){
-    var behavior = behavior=="map"?behavior:"fork";
-    return this.brapi_call(behavior,"get",function(datum){
-        var datum_params = typeof params === "function" ? params(datum) 
-                            : Object.assign({}, params);
-        var url = "/trials";
-        return {'url':url, 'params':datum_params};
-    }, typeof params === "function");
-};
+/** `GET /trials`
+ * @alias Context_Node.prototype.trials
+ * @param {Object} params Parameters to provide to the call
+ * @param {String} [behavior="fork"] Behavior of the node
+ * @return {BrAPI_Behavior_Node}
+ */
+export function trials (params,behavior){
+    var call = {
+        'defaultMethod': 'get',
+        'urlTemplate': '/trials',
+        'params': params,
+        'behaviorOptions': ['fork','map'],
+        'behavior': behavior,
+    }
+    this.version.check(call.urlTemplate,{
+        introduced:"v1.0"
+    });
+    return this.simple_brapi_call(call);
+}
 
-/** GET /trials/{trialDbId} */
-export function trials(params){
-    return this.brapi_call("map","get",function(datum){
-        var datum_params = typeof params === "function" ? params(datum) 
-                            : Object.assign({}, params);
-        var url = "/trials/"+datum_params.trialDbId;
-        delete datum_params.trialDbId;
-        return {'url':url, 'params':datum_params};
-    }, typeof params === "function");
-};
+/** `GET /trials/{trialDbId}`
+ * @alias Context_Node.prototype.trials_detail
+ * @param {Object} params Parameters to provide to the call
+ * @param {Object} params.trialDbId trialDbId
+ * @param {String} [behavior="fork"] Behavior of the node
+ * @return {BrAPI_Behavior_Node}
+ */
+export function trials_detail (params,behavior){
+    var call = {
+        'defaultMethod': 'get',
+        'urlTemplate': '/trials/{trialDbId}',
+        'params': params,
+        'behavior': 'map',
+    }
+    this.version.check(call.urlTemplate,{
+        introduced:"v1.0"
+    });
+    return this.simple_brapi_call(call);
+}
