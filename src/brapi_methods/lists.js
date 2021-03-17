5cdbcfad4b64cc7e88b30118ac9bc557f32506f6
Mirella Flores <mire_flor3@hotmail.com>
2021-03-17 16:48:31 -0400

    lists put and post methods

--- src/brapi_methods/lists.js
+++ src/brapi_methods/lists.js
@@ -18,6 +18,26 @@ export function lists (params,behavior){
     return this.simple_brapi_call(call);
 }
 
+/** `POST /lists`
+* @alias BrAPINode.prototype.lists_store
+* @param {Object} params Parameters to provide to the call
+* @param {String} [behavior="fork"] Behavior of the node
+* @return {BrAPI_Behavior_Node}
+*/
+export function lists_store (params,behavior){
+    var call = {
+        'defaultMethod': 'post',
+        'urlTemplate': '/lists',
+        'params': params,
+        'behaviorOptions': ['fork','map'],
+        'behavior': behavior,
+    }
+    this.version.check(call.urlTemplate,{
+        introduced:"v2.0"
+    });
+    return this.simple_brapi_call(call);
+}
+
 /** `GET /lists/{listDbId}`
  * @alias BrAPINode.prototype.lists_detail
  * @param {Object} params Parameters to provide to the call
@@ -37,6 +57,26 @@ export function lists_detail (params){
     return this.simple_brapi_call(call);
 }
 
+/** `PUT /lists/{listDbId}`
+* @alias BrAPINode.prototype.lists_modify
+* @param {Object} params Parameters to provide to the call
+* @param {String} [behavior="fork"] Behavior of the node
+* @return {BrAPI_Behavior_Node}
+*/
+export function lists_modify (params,behavior){
+    var call = {
+        'defaultMethod': 'put',
+        'urlTemplate': '/lists/{listDbId}',
+        'params': params,
+        'behaviorOptions': ['fork','map'],
+        'behavior': behavior,
+    }
+    this.version.check(call.urlTemplate,{
+        introduced:"v2.0"
+    });
+    return this.simple_brapi_call(call);
+}
+
 /** `PUT /lists/{listDbId}/items`
  * @alias BrAPINode.prototype.lists_items
  * @param {Object} params Parameters to provide to the call
@@ -51,7 +91,51 @@ export function lists_items (params){
         'behavior': 'map',
     }
     this.version.check(call.urlTemplate,{
-        introduced:"v1.3"
+        introduced:"v1.3",
+        deprecated:"v2.0"
     });
     return this.simple_brapi_call(call);
 }
+
+/** `POST /lists/{listDbId}/items`
+ * @alias BrAPINode.prototype.lists_store_items
+ * @param {Object} params Parameters to provide to the call
+ * @param {String} params.imageDbId imageDbId
+ * @return {BrAPI_Behavior_Node}
+ */
+export function lists_store_items (params){
+    var call = {
+        'defaultMethod': 'post',
+        'urlTemplate': '/lists/{listDbId}/items',
+        'params': params,
+        'behavior': 'map',
+    }
+    this.version.check(call.urlTemplate,{
+        introduced:"v2.0"
+    });
+    return this.simple_brapi_call(call);
+}
+
+
+/** `POST /search/lists`
+* @alias BrAPINode.prototype.lists_search
+* @param {Object} params Parameters to provide to the call
+* @param {String} [behavior="fork"] Behavior of the node
+* @return {BrAPI_Behavior_Node}
+*/
+export function lists_search(params,behavior){
+    return this.search_lists(params,behavior,true);
+};
+
+/** `POST /search/lists -> GET /search/lists`
+* @alias BrAPINode.prototype.search_lists
+* @param {Object} params Parameters to provide to the call
+* @param {String} [behavior="fork"] Behavior of the node
+* @return {BrAPI_Behavior_Node}
+*/
+export function search_lists(params,behavior,useOld){
+    this.version.check("POST /search/lists -> GET /search/lists",{
+        introduced:"v2.0"
+    });
+    return this.search("lists",params,behavior);
+};
