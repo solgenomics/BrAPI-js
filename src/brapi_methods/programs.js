b5ef5d43c0160a6f5a0d37954445aed52df2cbde
Mirella Flores <mire_flor3@hotmail.com>
2021-03-17 16:07:07 -0400

    programs put and post methods

--- src/brapi_methods/programs.js
+++ src/brapi_methods/programs.js
@@ -18,6 +18,65 @@ export function programs (params,behavior){
     return this.simple_brapi_call(call);
 }
 
+/** `POST /programs`
+* @alias BrAPINode.prototype.programs_store
+* @param {Object} params Parameters to provide to the call
+* @param {String} [behavior="fork"] Behavior of the node
+* @return {BrAPI_Behavior_Node}
+*/
+export function programs_store (params,behavior){
+    var call = {
+        'defaultMethod': 'post',
+        'urlTemplate': '/programs',
+        'params': params,
+        'behaviorOptions': ['fork','map'],
+        'behavior': behavior,
+    }
+    this.version.check(call.urlTemplate,{
+        introduced:"v2.0"
+    });
+    return this.simple_brapi_call(call);
+}
+
+/** `GET /programs/{programDbId}`
+ * @alias BrAPINode.prototype.programs_detail
+ * @param {Object} params Parameters to provide to the call
+ * @param {String} params.germplasmDbId germplasmDbId
+ * @return {BrAPI_Behavior_Node}
+ */
+export function programs_detail (params){
+    var call = {
+        'defaultMethod': 'get',
+        'urlTemplate': '/programs/{programDbId}',
+        'params': params,
+        'behavior': 'map',
+    }
+    this.version.check(call.urlTemplate,{
+        introduced:"v2.0"
+    });
+    return this.simple_brapi_call(call);
+}
+
+/** `PUT /programs/{programDbId}`
+* @alias BrAPINode.prototype.programs_modify
+* @param {Object} params Parameters to provide to the call
+* @param {String} [behavior="fork"] Behavior of the node
+* @return {BrAPI_Behavior_Node}
+*/
+export function programs_modify (params,behavior){
+    var call = {
+        'defaultMethod': 'put',
+        'urlTemplate': '/programs/{programDbId}',
+        'params': params,
+        'behaviorOptions': ['fork','map'],
+        'behavior': behavior,
+    }
+    this.version.check(call.urlTemplate,{
+        introduced:"v2.0"
+    });
+    return this.simple_brapi_call(call);
+}
+
 /** `POST /programs-search`
 * @alias BrAPINode.prototype.programs_search
 * @param {Object} params Parameters to provide to the call
@@ -45,7 +104,8 @@ export function search_programs(params,behavior,useOld){
         call.defaultMethod = "post";
         this.version.check(call.urlTemplate,{
             introduced:"v1.0",
-            deprecated:"v1.3"
+            deprecated:"v1.3",
+            deprecated:"v2.0"
         });
         return this.simple_brapi_call(call);
     } else {
