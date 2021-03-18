4cdc2104ec1e30e27323d4b71cba7b7c810eb95f
Mirella Flores <mire_flor3@hotmail.com>
2021-03-18 09:55:47 -0400

    traits put and post methods

--- src/brapi_methods/traits.js
+++ src/brapi_methods/traits.js
@@ -18,6 +18,26 @@ export function traits (params,behavior){
     return this.simple_brapi_call(call);
 }
 
+/** `POST /traits`
+* @alias BrAPINode.prototype.traits_store
+* @param {Object} params Parameters to provide to the call
+* @param {String} [behavior="fork"] Behavior of the node
+* @return {BrAPI_Behavior_Node}
+*/
+export function traits_store (params,behavior){
+    var call = {
+        'defaultMethod': 'post',
+        'urlTemplate': '/traits',
+        'params': params,
+        'behaviorOptions': ['fork','map'],
+        'behavior': behavior,
+    }
+    this.version.check(call.urlTemplate,{
+        introduced:"v2.0"
+    });
+    return this.simple_brapi_call(call);
+}
+
 /** `GET /traits/{traitDbId}`
  * @alias BrAPINode.prototype.traits_detail
  * @param {Object} params Parameters to provide to the call
@@ -33,6 +53,26 @@ export function traits_detail (params){
     }
     this.version.check(call.urlTemplate,{
         introduced:"v1.0"
+    });
+    return this.simple_brapi_call(call);
+}
+
+/** `PUT /traits/{traitDbId}`
+* @alias BrAPINode.prototype.traits_modify
+* @param {Object} params Parameters to provide to the call
+* @param {String} [behavior="fork"] Behavior of the node
+* @return {BrAPI_Behavior_Node}
+*/
+export function traits_modify (params,behavior){
+    var call = {
+        'defaultMethod': 'put',
+        'urlTemplate': '/traits/{traitDbId}',
+        'params': params,
+        'behaviorOptions': ['fork','map'],
+        'behavior': behavior,
+    }
+    this.version.check(call.urlTemplate,{
+        introduced:"v2.0"
     });
     return this.simple_brapi_call(call);
 }
